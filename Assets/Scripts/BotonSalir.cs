df141248bcf88a59e19a28f3298d3f0dadd5e149
Juan Esteban Alvarez Mesa <juanito10997@gmail.com>
2016-11-09 12:01:22 -0500

    proyectoFinalizado
    Este proyecto ya está listo

--- Assets/Scripts/BotonSalir.cs
+++ Assets/Scripts/BotonSalir.cs
@@ -5,11 +5,12 @@ using UnityEngine.SceneManagement;
 public class BotonSalir : MonoBehaviour {
 
 	public bool salir;
+	public ElementoInteractivo ImagenSalir;
 	
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.GetKeyDown(KeyCode.Escape))
+		if (Input.GetKeyDown(KeyCode.Escape)||ImagenSalir.pulsado)
 		{
 			if (salir) {
 				Debug.Log ("Salimos del juego");
@@ -17,7 +18,7 @@ public class BotonSalir : MonoBehaviour {
 			} 
 			else
 			{
-				SceneManager.LoadScene("Portada");
+				SceneManager.LoadScene("MainMenu");
 			}
 		}
 
