51f938a9e6fc6a49f6b017d0b91cb3753e4c8b06
Juan Esteban Alvarez Mesa <juanito10997@gmail.com>
2016-11-09 00:14:09 -0500

    barra que se enoje
    Este commit es hecho por juanchon y hace que la barra se encoja al destruirse un bloque

--- Assets/Scripts/Barra/Barra.cs
+++ Assets/Scripts/Barra/Barra.cs
@@ -3,27 +3,34 @@ using System.Collections;
 
 public class Barra : MonoBehaviour {
 
+	//Hola profe para hacer que la barra fuera cada vez más pequeña fue relativamente sencillo, lo unico que tuve que hacer fue que cuando se produgera
+	// el evento del contador de bloques que dice cuando hay un bloque menos se suscribiera el metodo de ReducirTamaño
 	public float velocidad = 20f;
 
 	Vector3 posicionInicial;
+	Vector3 TamañoInicial;
 
-	// Use this for initialization
+
 	void Start () {
 		
 		posicionInicial = transform.position;
+		TamañoInicial = transform.localScale;
+		// acá cuando se escuche el evento el metodo se suscribe y se ejecuta
         Puntuacion.OnLevelCompleted += DisableGameObject;
         Vidas2.OnNoMoreLifes += DisableGameObject;
+		ContadorBloques.OnBloqueDestroy += ReducirTamaño;  
 	}
 
 	public void Reset()
 	{
 		transform.position = posicionInicial;
 	}
-	// Update is called once per frame
-	void Update () { // se ejecuta el codigo tantas veces como segundos haya
+
+	void Update () { 
 		float tecladoHorizontal = Input.GetAxisRaw("Horizontal");
 		float posX = transform.position.x + (tecladoHorizontal* velocidad * Time.deltaTime);
-		transform.position = new Vector3 (Mathf.Clamp(posX,-8,8),transform.position.y,transform.position.z);
+		transform.position = new Vector3 (Mathf.Clamp (posX, -8, 8), transform.position.y, transform.position.z);
+		//El mathf.Clamp hace que la posicion solo oxile entre esos así, esto para limitar que la barra se salga o supere las barreras (limites). 
 	}
 
     void DisableGameObject()
@@ -32,16 +39,21 @@ public class Barra : MonoBehaviour {
     }
 
     void OnDestroy()
-    {
-        // Dado que los eventos a los que nos subscribimos son estaticos
-        // es decir, pertenecen a la clase y no a la instancia, cuando la escena
+    {	//
+        // Dado que los eventos a los que nos subscribimos pertenecen a la clase y no a la instancia, cuando la escena
         // finaliza la instancia se destruye y este codigo va a intentar 
         // seguir accediendo al mismo evento aunque ya ha sido destruido
         // para evitar esto y hacer que se actualice en cada escena
-        // es necesario desubscribirme de los eventos estaticos cuando la escena finaliza
-        // Keep in mind that since static members don't belong to a specific instance, the variable only exists once and is shared between all instances of that class.
-        // dado que cuando finaliza la escena se pierden todas las instancias la variable estatica de la funcion deja de existir
+        // es necesario desubscribirme de los eventos  cuando la escena finaliza
+        // dado que cuando finaliza la escena se pierden todas las instancias la variable de la funcion deja de existir
         Puntuacion.OnLevelCompleted -= DisableGameObject;
         Vidas2.OnNoMoreLifes -= DisableGameObject;
+		ContadorBloques.OnBloqueDestroy -= ReducirTamaño;
+
     }
+
+	void ReducirTamaño()
+	{
+		transform.localScale = new Vector3 ((transform.localScale.x)- 0.08f, transform.localScale.y, transform.localScale.z);// reduce un poco el tamaño de la barra con respecto al eje x y los otros dos ejes los deja como estaba
+	}
 }
--- Assets/Scripts/Bloques/ContadorBloques.cs
+++ Assets/Scripts/Bloques/ContadorBloques.cs
@@ -3,29 +3,31 @@ using System.Collections;
 
 
 public class ContadorBloques : MonoBehaviour {
-    public delegate void ContadorDeBloques();
-    public static event ContadorDeBloques OnBloqueDestroy;
-    public static event ContadorDeBloques OnZeroBloques;
+    public delegate void ContadorDeBloques(); // se crea el delegado 
+    public static event ContadorDeBloques OnBloqueDestroy; // se crea el evento que avisa cuando hay un bloque menos. 
+    public static event ContadorDeBloques OnZeroBloques; // se crea el evento que avisa cuando no hay mas bloques.
+	  
 
 	private bool ContadorSonido = true;
 
     private int countInitialBloques;
 
-	// Use this for initialization
+
 	void Start () {
-        countInitialBloques = gameObject.GetComponent<Transform>().childCount;
+        countInitialBloques = gameObject.GetComponent<Transform>().childCount; // se asocia el numero de hijos al contadorInicialBloques para saber cuantos bloques hay
         Debug.Log(countInitialBloques);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        int actualBloquesCount = gameObject.GetComponent<Transform>().childCount;
+        int actualBloquesCount = gameObject.GetComponent<Transform>().childCount; // se actualizan la cantidad de hijos que hay 
 		//Debug.Log ("Contador de bloques: " + actualBloquesCount);
 		if (actualBloquesCount < countInitialBloques)
         {
+			// si la cantidad de hijos inicial es mayor a la actual es porque un bloque se destruyó y se llama el evento de que se destruyó un bloque.
             countInitialBloques = gameObject.GetComponent<Transform>().childCount;
             //Debug.Log("Un bloque se destruyo");
-            OnBloqueDestroy();
+            OnBloqueDestroy(); 
         }
 
 		if (actualBloquesCount == 0 && ContadorSonido == true)
